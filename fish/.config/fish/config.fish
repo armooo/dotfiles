1e389ef8df662159e491738c1a3491929ac85fdd
Jason Michalski <armooo@armooo.net>
2015-08-03 21:48:01 -0700

    Add virtualfish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,5 @@ function my_key_bindings
     bind -M insert -m default jj force-repaint
 end
 set -g fish_key_bindings my_key_bindings
+
+eval (python ~/.config/fish/virtualfish/virtualfish/)
--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -45,6 +45,12 @@ function fish_prompt --description 'Write out the prompt'
 		end
 	end
 
+    function virtual_env
+        if set -q VIRTUAL_ENV
+            echo "["(basename "$VIRTUAL_ENV")"]"
+        end
+    end
+
 	set -l color_cwd
 	set -l prefix
 	switch $USER
@@ -87,5 +93,5 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) (virtual_env) $normal $prompt_status "$mode_str" "> "
 end
