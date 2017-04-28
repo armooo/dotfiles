66ff846ef8eb28f835a63eb9162f4d29d075b11a
Jason Michalski <armooo@armooo.net>
2017-04-28 22:04:57 +0000

    Fix brun

--- fish/.config/fish/functions/brun.fish
+++ fish/.config/fish/functions/brun.fish
@@ -2,7 +2,13 @@ function brun
     pushd ~/server
     bazel build $argv[1]
     popd
-    set bin (string replace // ~/server/bazel-bin/ (string replace : / $argv[1]))
-    eval $bin $argv[2..-1]
+    if string match '*:*' $argv[1]
+        set bin (string replace // ~/server/bazel-bin/ (string replace : / $argv[1]))
+    else
+        set last (string split --max 1 --right / $argv[1])[2]
+        set bin (string replace // ~/server/bazel-bin/ $argv[1])/$last
+    end
+    set args $argv[2..-1]
+    eval $bin $args
 end
 
