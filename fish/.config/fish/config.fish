b59c885146c7b6aa6733107c7b338e3b93326378
Jason Michalski <armooo@armooo.net>
2024-12-02 13:08:38 -0800

    Add sway

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,9 @@
-# Key bindings
-function my_key_bindings
-    fish_vi_key_bindings
-    bind -M insert -m default jj force-repaint
+if status is-interactive
+    # Key bindings
+    function my_key_bindings
+        fish_vi_key_bindings
+        bind -M insert -m default jj force-repaint
+    end
+    set -g fish_key_bindings my_key_bindings
+    
 end
-set -g fish_key_bindings my_key_bindings
-
-eval (python3 ~/.config/fish/virtualfish/virtualfish/)
